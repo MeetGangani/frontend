664b496e25aa615e61c065f2f0b375705298efa5
MeetGangani <meetgangani56@gmail.com>
2025-02-22 12:15:09 +0530

    test

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -31,7 +31,7 @@ const Hero = () => {
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.8 }}
-            className="text-center"
+            className="text-center" 
           >
             {/* Badge */}
             <motion.div
